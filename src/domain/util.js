908fd7b03405de24372e919e172870f2de7e5341
ulxsth <ulxsth@gmail.com>
2025-02-06 09:05:31 +0900

    chore: startsWithHiragana を定義

--- src/domain/util.js
+++ src/domain/util.js
@@ -7,3 +7,13 @@
 function getRandomAI() {
   return "愛"
 }
+
+/**
+ * 文字列が、指定されたひらがなから始まっているかを確かめる関数。
+ * @param {string} text 検査したい文字列
+ * @param {string} excepted 頭にくることが期待されている文字（例：あ、い）
+ * @return {boolean} 指定した文字で始まっているかどうか
+ */
+function startsWithHiragana(text, excepted) {
+  return true;
+}
