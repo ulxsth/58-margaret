577add629365335eaf972f582b74b301d1008355
hayashu <kindsun884.730@gmail.com>
2025-02-07 01:19:36 +0900

    「あい」に関するデータ取得関数作成

--- src/domain/util.js
+++ src/domain/util.js
@@ -4,9 +4,44 @@
  *
  * @return 「あい」から始まる単語
  */
-function getRandomAI() {
-	return "愛";
+
+async function getRandomAI() {
+	// 取得するページ番号の最大値
+	const maxPage = 170;
+	// URLのキーワード，ページ番号決定
+	const keyWord = 'あい';
+	const randomPage = Math.floor(Math.random() * maxPage) + 1;
+
+	const apiURL = `https://jisho.org/api/v1/search/words?keyword=${keyWord}&page=${randomPage}`;
+
+	try {
+		// APIによる情報取得
+		const response = await fetch(apiURL, {method: 'GET',headers: {
+			"Content-Type": "application/json"
+		}});
+		// データをjson形式化
+		const jsonData = await response.json();
+
+		if (jsonData){
+			// データ長さ
+			const dataNums = jsonData.data.length
+			// ページからランダムに単語取得(読み，単語)
+			const randomItem = Math.floor(Math.random() * dataNums);
+			const {word,reading} = jsonData.data[randomItem].japanese[0];
+			// const slug = jsonData.data[randomItem].slug;
+	
+			// TODO: readingから，「・」，「=」の除去
+			return {word, reading};
+		}
+		else{
+			return console.error(`Error: ${response.status}`);
+		}
+	} catch (error) {
+		return console.error(error);
+	}
 }
+// テスト用コード(単語，読み)
+console.log(await getRandomAI())
 
 /**
  * 文字列が、指定されたひらがなから始まっているかを確かめる関数。
