6ba9e17bd532ee7a54d3a62fb780f872c56c008d
ulxsth <ulxsth@gmail.com>
2025-02-08 15:56:32 +0900

    test: green/'あいうえお'に対し'あ'が渡されたとき、trueを返す

--- tests/units/util.test.js
+++ tests/units/util.test.js
@@ -2,6 +2,10 @@ import { startsWithHiragana } from "../../src/domain/util";
 
 describe("startsWithHiragana は文字列の読みをそのままにして平仮名のみに置換する", () => {
   test("'あいうえお'に対し'あ'が渡されたとき、trueを返す", () => {
-    throw new Error("実装されていません");
+    // given, when
+    const result = startsWithHiragana("あいうえお", "あ");
+
+    // then
+    expect(result).toBe(true);
   });
 });
