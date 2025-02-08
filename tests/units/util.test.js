827167766e61da0f57626305d349fc272078f6a0
ulxsth <ulxsth@gmail.com>
2025-02-08 16:33:47 +0900

    chore: change working directory

--- tests/units/util.test.js
+++ tests/units/util.test.js
@@ -1,35 +1,47 @@
 import { startsWithHiragana } from "../../src/domain/util";
 
 describe("startsWithHiragana: 指定した平仮名から始まる文字列かを検査する", () => {
-  test("'あいうえお', 'あ' => true", async () => {
-    // given, when
-    const result = await startsWithHiragana("あいうえお", "あ");
+  describe("正常系", () => {
+    test("'あいうえお', 'あ' => true", async () => {
+      // given, when
+      const result = await startsWithHiragana("あいうえお", "あ");
 
-    // then
-    expect(result).toBe(true);
-  });
+      // then
+      expect(result).toBe(true);
+    });
 
-  test("'あいうえお', 'い' => false", async () => {
-    // given, when
-    const result = await startsWithHiragana("あいうえお", "い");
+    test("'あいうえお', 'い' => false", async () => {
+      // given, when
+      const result = await startsWithHiragana("あいうえお", "い");
 
-    // then
-    expect(result).toBe(false);
-  });
+      // then
+      expect(result).toBe(false);
+    });
 
-  test("'色は匂へど散りぬるを', 'い' => true", async () => {
-    // given, when
-    const result = await startsWithHiragana("色は匂へど散りぬるを", "い");
+    test("'色は匂へど散りぬるを', 'い' => true", async () => {
+      // given, when
+      const result = await startsWithHiragana("色は匂へど散りぬるを", "い");
 
-    // then
-    expect(result).toBe(true);
-  });
+      // then
+      expect(result).toBe(true);
+    });
 
-  test("'色は匂へど散りぬるを', 'あ' => false", async () => {
-    // given, when
-    const result = await startsWithHiragana("色は匂へど散りぬるを", "あ");
+    test("'色は匂へど散りぬるを', 'あ' => false", async () => {
+      // given, when
+      const result = await startsWithHiragana("色は匂へど散りぬるを", "あ");
 
-    // then
-    expect(result).toBe(false);
-  });
+      // then
+      expect(result).toBe(false);
+    });
+  })
+
+  describe("異常系", () => {
+    test("'', 'あ' => IllegalArgumentError", async () => {
+      // given, when
+      const result = startsWithHiragana("", "あ");
+
+      // then
+      await expect(result).rejects.toThrow("引数が不正です");
+    });
+  })
 });
