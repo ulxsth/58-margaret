64067723587e3ea82117aeba9bd55f66e80e4ff9
hayashu <kindsun884.730@gmail.com>
2025-02-13 20:55:37 +0900

    groqの追加とテスト(仮)作成

--- src/infra/LLM.js
+++ src/infra/LLM.js
@@ -2,7 +2,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import { GoogleGenerativeAI } from "@google/generative-ai";
-
+import Groq from "groq-sdk";
 /**
  * LLM API に問い合わせる関数。
  * @param {string} model - 使用するモデルの名前。
@@ -17,6 +17,8 @@ export async function askLLM(model, prompt, options = {}) {
 			return await askGPT(prompt, options);
 		case "gemini":
 			return await askGemini(prompt,options);
+		case "groq":
+			return await askGroq(prompt, options);
 		default:
 			throw new Error(`Unsupported model: ${model}`);
 	}
@@ -26,7 +28,6 @@ async function askGPT() {}
 
 // gemini
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-
 export async function askGemini(prompt){
 	try {
 		const model = genAI.getGenerativeModel({ model: "gemini-pro"});
@@ -39,3 +40,20 @@ export async function askGemini(prompt){
 	}
 }
 
+// groq api
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+export async function askGroq(prompt){
+	try {
+		return groq.chat.completions.create({
+			messages: [
+				{
+					role: "user",
+					content: prompt
+				},
+			],
+			model: "llama-3.3-70b-versatile",
+		});
+	} catch (error) {
+		return error;
+	}
+}
