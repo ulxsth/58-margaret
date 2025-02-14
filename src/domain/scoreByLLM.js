92ba61da02fef40a45796de13f15a117f84dce35
hayashu <kindsun884.730@gmail.com>
2025-02-15 00:28:11 +0900

    一時保存

--- src/domain/scoreByLLM.js
+++ src/domain/scoreByLLM.js
@@ -1,8 +1,23 @@
+import askLLM from '../infra/LLM.js';
+
 /**
  * あいうえお作文の採点を行う。
  * @param {string} text ユーザーが書いたあいうえお作文
  * @return {Map<string, number[]>} 各LLMの名前と、LLMの採点結果（3項目それぞれ）を持つMap
  */
+
+const useLLM = ['gemini', 'groq'];
+let results = {};
 function score(text) {
-	return 100;
+	for (i=0; i < useLLM.length; i++) {
+		const res = askLLM(useLLM[i], text);
+		const result = {
+			name : useLLM[i],
+			scores : res
+		}
+		results = {...results, result};
+	}
+	return results;
 }
+
+score(text);
