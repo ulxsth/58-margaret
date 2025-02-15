c196fccf3aac0655d984aaf90289864ef605d952
ulxsth <ulxsth@gmail.com>
2025-02-16 05:33:52 +0900

    fix: score import 忘れを修正

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import expressEjsLayouts from "express-ejs-layouts";
 import { convertToYomi, getRandomAI } from "./domain/util.js";
+import { score } from "./domain/scoreByLLM.js";
 
 const PORT = 8080;
 
