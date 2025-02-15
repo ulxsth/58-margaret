53084bddbca6464dc3dbb4286b32efda039995dc
ulxsth <ulxsth@gmail.com>
2025-02-16 06:47:21 +0900

    feat: prefix を読みに変更した

--- src/app.js
+++ src/app.js
@@ -23,7 +23,10 @@ app.get("/", (req, res) => {
 
 app.get("/aiueo-sakubun/input", async (req, res) => {
 	const data = await getRandomAI();
-	res.render("aiueo-sakubun/input", { data: data });
+	const word = data.japanese[0].word ?? data.japanese[0].reading;
+	const yomi = await convertToYomi(word);
+
+	res.render("aiueo-sakubun/input", { word, yomi });
 });
 
 app.get("/aiueo-sakubun/result", (req, res) => {
