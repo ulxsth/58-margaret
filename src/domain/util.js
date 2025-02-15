1a6540153c2be3b9665816eee45f86936d61fcc3
ulxsth <ulxsth@gmail.com>
2025-02-16 07:42:37 +0900

    test: 仕様変更に伴うテストの変更

--- src/domain/util.js
+++ src/domain/util.js
@@ -55,7 +55,8 @@ export async function startsWithHiragana(text, excepted) {
 	}
 
 	const yomi = await convertToYomi(text);
-	return yomi.startsWith(excepted);
+	const exceptedKana = hiraToKana(excepted);
+	return yomi.startsWith(exceptedKana);
 }
 
 /**
@@ -76,6 +77,13 @@ export async function convertToYomi(text) {
 function kanaToHira(str) {
 	return str.replace(/[\u30a1-\u30f6]/g, (match) => {
 		const chr = match.charCodeAt(0) - 0x60;
+		return String.fromCharCode(chr);
+	});
+}
+
+function hiraToKana(str) {
+	return str.replace(/[\u3041-\u3096]/g, (match) => {
+		const chr = match.charCodeAt(0) + 0x60;
 		return String.fromCharCode(chr);
 	});
 }
