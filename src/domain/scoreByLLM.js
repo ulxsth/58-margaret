8d971f39ad8a4dcf9d7b8a7f3f89b4c6ae02ffb3
hayashu <kindsun884.730@gmail.com>
2025-02-15 21:35:27 +0900

    format:fix

--- src/domain/scoreByLLM.js
+++ src/domain/scoreByLLM.js
@@ -43,7 +43,7 @@ export async function score(formData) {
 const formData = {
 	topic: "あいしてる",
 	text: ["あなたを", "いつも", "しょっちゅう", "ていきてきに", "るーぷする"],
-}
+};
 
 // const output = await score(formData);
 // console.log(output.gemini);
