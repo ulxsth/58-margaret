9902efdf4e3d2dc1efc451e76e1e3614b71cff22
hayashu <kindsun884.730@gmail.com>
2025-02-15 23:58:00 +0900

    LLMによる採点を修正(app.js)

--- src/app.js
+++ src/app.js
@@ -27,9 +27,10 @@ app.get("/api/v1/word", async (req, res) => {
 	res.json(data);
 });
 
-app.get("/api/v1/score", async (req, res) => {
-	const scores = await getRandomAI(req.body);
-	res.json(scores);
+app.post("/api/v1/score", async (req, res) => {
+	const scores = await score(req.body);
+	// console.log(scores);
+	res.send(scores);
 });
 
 app.post("/api/v1/starts-with", (req, res) => {
